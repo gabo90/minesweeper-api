5ab749bff8dc1b31bc65f185a2d2b34a63e68086
Gabriel Geronimo <ggeronimoguerra@gmail.com>
2021-07-26 13:31:58 -0500

    refactoring for api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :cells
-  resources :boards
-  resources :games
-  resources :players
+  namespace :api do
+    namespace :v1 do
+      resources :cells
+      resources :boards
+      resources :games
+      resources :players
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
